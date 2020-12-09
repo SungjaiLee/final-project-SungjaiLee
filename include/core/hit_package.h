7a93cf5730b906d2cc71437be042d21031579e36
jack <sungjai2@illinois.edu>
2020-12-09 20:12:51 +0900

    94. HitPackage header and source cleaned up.

--- include/core/hit_package.h
+++ include/core/hit_package.h
@@ -13,24 +13,46 @@
 namespace room_explorer {
 
 /**
- * Container of all the hits intersected.
- * Allow merging and manipulating of distances of hits contained.
+ * Summary of all the hits intersected by a ray in a single direction.
+ * Allows merging of two hit-packages.
+ * Allow simple shifting and scaling of distances of all the hits in the package.
  */
 class HitPackage {
+private:
   std::map<float, Hit> hits_;
 
 public:
+  // Getters =======================================================
   size_t HitCount() const;
 
   const std::map<float, Hit>& GetHits() const;
+  // End of Getters ================================================
 
-  bool AddHit(Hit hit);
+  // Package Addition Methods ==========================================================================================
+  /**
+   * Add passed hit into the package.
+   * If a hit already exist at the given distance, hit with higher priority will take over.
+   * Does not add invalid hits.
+   * @param hit Constant reference to hit to be added.
+   * @return True if hit was able to be added. False if failed, be it due to existing hit, or invalid hit,
+   */
+  bool AddHit(const Hit& hit);
 
+  /**
+   * Merges two hit packages into the current package.
+   * If a hit already exist at the given distance, hit with higher priority will take over.
+   *    Hit from this package is prioritized over the hits from other.
+   * @param package Constant reference of the package to be merged into this package.
+   */
   void Merge(const HitPackage& package);
+  // End of Package Addition Methods ===================================================================================
 
+
+  // Distance Manipulator ==============================================================================================
   void ShiftHits(float shift);
 
   void ScaleDistances(float scale);
+  // End of Distance Manipulator =======================================================================================
 };
 
 } // namespace room_explorer
--- src/core/hit_package.cc
+++ src/core/hit_package.cc
@@ -6,6 +6,7 @@
 
 namespace room_explorer {
 
+// Getters ==============================================================================
 size_t HitPackage::HitCount() const {
   return hits_.size();
 }
@@ -13,67 +14,67 @@ size_t HitPackage::HitCount() const {
 const std::map<float, Hit>& HitPackage::GetHits() const {
   return hits_;
 }
+// Getters ==============================================================================
 
-bool HitPackage::AddHit(Hit hit) {
+
+// Package Addition Methods ============================================================================================
+bool HitPackage::AddHit(const Hit& hit) {
+  // Ignore invalid hits
   if (hit.IsNoHit()) {
     return false;
   }
-  if (hits_.find(hit.hit_distance_) != hits_.end()) {
-    Hit prev = hits_[hit.hit_distance_];
 
+  // If a hit already exists on the given distance, one with higher priority should take over
+  if (hits_.find(hit.hit_distance_) != hits_.end()) {
+    const Hit& prev = hits_[hit.hit_distance_];
+
+    // Only replace the hit if priority if strictly higher
     if (hit.hit_type_ > prev.hit_type_) {
-      // If eual priority, leave th eprevious one
       hits_[hit.hit_distance_] = hit;
       return true;
+
     } else {
       return false;
     }
   }
-
 
   hits_[hit.hit_distance_] = hit;
   return true;
 }
 
 void HitPackage::Merge(const HitPackage& package) {
-  auto hit_map = package.GetHits();
-  auto hit_map_iterator = hit_map.begin();
-
-  while (hit_map_iterator != hit_map.end()) {
-    this->AddHit(hit_map_iterator->second);
-    ++hit_map_iterator;
+  // const auto& is pair of float and Hit
+  for (const auto& hit_pair : package.GetHits()) {
+    AddHit(hit_pair.second); // in the
   }
-
 }
+// End of  Package Addition Methods ====================================================================================
 
+
+// Distance Manipulator ================================================================================================
 void HitPackage::ShiftHits(float shift) {
-  auto temp_hit = std::move(hits_);
-  // assume hits_ is not cleared
-  assert(hits_.empty());
+  // To shift the maps back, first remove the current map, then add shifted hits back
+  auto temp_hits = std::move(hits_);
 
-  auto temp_hit_it = temp_hit.begin();
-  while (temp_hit_it != temp_hit.end()) {
-
-    Hit hit(temp_hit_it->second);
+  // Add all the hits back, after shifting them
+  for(auto& hit_pairs : temp_hits) {
+    Hit& hit{hit_pairs.second};
     hit.ShiftDistance(shift);
-    AddHit(hit);
-    ++temp_hit_it;
+    AddHit(hit); // Add Back using usual method
   }
 }
 
 void HitPackage::ScaleDistances(float scale) {
-  auto temp_hit = std::move(hits_);
-  // assume hits_ is not cleared
-  assert(hits_.empty());
+  // To shift the maps back, first remove the current map, then add scaled hits back
+  auto temp_hits = std::move(hits_);
 
-  auto temp_hit_it = temp_hit.begin();
-  while (temp_hit_it != temp_hit.end()) {
-
-    Hit hit(temp_hit_it->second);
+  // Add all the hits back, after scaling them
+  for(auto& hit_pairs : temp_hits) {
+    Hit& hit{hit_pairs.second};
     hit.ScaleDistance(scale);
-    AddHit(hit);
-    ++temp_hit_it;
+    AddHit(hit); // Add Back using usual method
   }
 }
+// End of Distance Manipulator =========================================================================================
 
 } //namespace room_explorer
